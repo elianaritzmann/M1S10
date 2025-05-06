43f755cc101a5bb3973493abb407da0f5a68ae2d
eliananana1 <eliana.cristinas2@hotmail.com>
2025-05-06 18:13:05 -0300

    atualizacao

--- src/main/java/M1S10/M1S10/controllers/LoginController.java
+++ src/main/java/M1S10/M1S10/controllers/LoginController.java
@@ -2,17 +2,18 @@ package M1S10.M1S10.controllers;
 
 import M1S10.M1S10.dtos.LoginRequestDto;
 import M1S10.M1S10.dtos.LoginResponseDto;
-import M1S10.M1S10.services.LoginServiceImpl;
+import M1S10.M1S10.services.LoginService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@AllArgsConstructor
 @RestController
 @RequestMapping("login")
 public class LoginController {
 
-    private LoginServiceImpl service;
+    private LoginService service;
 
     @PostMapping
     private LoginResponseDto login(@RequestBody LoginRequestDto dto){
